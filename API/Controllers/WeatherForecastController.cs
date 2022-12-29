0b28c88b22e576da4611471a044f299aec547107
hakim bendiabdallah <hakim.bendiabdallah@gmail.com>
2022-12-29 12:04:48 +0100

    Ajoutez des fichiers projet.

---
+++ API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "hakim","Freezingss", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Swelterings", "Scorchings"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[0]
+        })
+        .ToArray();
+    }
+
+    
+
+
+
+
+
+}
